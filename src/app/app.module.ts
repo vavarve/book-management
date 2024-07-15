b94e0d0eb03f42e56f0eeef47a95861596dc6dda
Vasilis Varveris <vasilis.v@itrust.digital>
2024-07-15 08:54:27 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './books/book.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { AppState } from './app.state';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './books/book.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BookListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot<AppState>({ book: BookReducer }),
+    EffectsModule.forRoot([BookEffects]),
+    StoreDevtoolsModule.instrument(),
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
